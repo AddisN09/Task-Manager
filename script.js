eeea2d2ce05eae9fcc64ac187b8513eea4051329
Addis Negash <anegash154@gmail.com>
2025-06-05 21:14:15 +0300

    feat:add a renderTaskes function in the js
    It checkes whether the input filed is empety or not
    It iterate over each array element and create a list tag for each tag
    displa the inserted text inthe form of task by using textContent
    if the task is checked it will add the checked class

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 const list_container = document.getElementById('u-list');
 const button = document.getElementById('but');
 let tasks = [];
+function renderTasks() {
+  list_container.innerHTML = ""; 
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.textContent = task.text;
+
+    if (task.checked) {
+      li.classList.add('checked');
+    }
+  });
+}
