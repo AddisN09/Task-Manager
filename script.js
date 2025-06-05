ed699b57690f714674a2115010e3dc8772545bc9
Addis Negash <anegash154@gmail.com>
2025-06-05 21:07:19 +0300

    feat:add array fot the task storage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
  const input_box = document.getElementById('input-filed');
 const list_container = document.getElementById('u-list');
 const button = document.getElementById('but');
+let tasks = [];
